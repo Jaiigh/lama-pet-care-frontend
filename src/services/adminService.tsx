7ba831add21e13aef2d33dbf41132854e1cc17b6
tatthon <tatthon.limmonthol@gmail.com>
2025-11-15 18:52:17 +0700

    feat-add admin edit user page

---
+++ src/services/adminService.tsx
@@ -0,0 +1,20 @@
+const getProfileByAdmin = async (userId: string ,adminToken: string) => {
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE}/admin/users/${userId}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${adminToken}`,
+            },
+        });
+        if (!response.ok) {
+            throw new Error("Failed to fetch profile by admin");
+        }
+        const data = await response.json();
+        return data;
+    }catch (err) {
+        console.error("Error fetching profile by admin:", err);
+        throw err;
+    }
+};
+export default getProfileByAdmin;
